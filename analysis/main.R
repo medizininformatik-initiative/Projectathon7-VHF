0c98419e999df6248105f128a262d3e29878fb10
Alexander Strübing <alexander.struebing@gmail.com>
2022-10-10 21:39:47 +0200

    Add Basic Analyis Options Skeleton
    This only adds a for loop, indentation and some future used option values.

--- analysis/main.R
+++ analysis/main.R
@@ -208,156 +208,179 @@ for (runOption in runOptions) {
   ####################################
   # Start Analysis from S. Zeynalova #
   ####################################
-  
-  resultRows <- nrow(result)
 
-  # check possible data problems
-  errorMessage <- ""
-  # not enough data rows 
-  if (resultRows < 2) {
-    errorMessage <- paste0("Result table has ", resultRows, " rows -> abort analysis\n")
-  }
-  if (all(result$Vorhofflimmern == result$Vorhofflimmern[1])) { # only 0 or only 1 in this diagnosis column
-    errorMessage <- paste0("All Vorhofflimmern diagnoses have the same value ", result$Vorhofflimmern[1], " -> abort analysis\n")
-  }
-  hasError <- nchar(errorMessage) > 0
+  # Order to run the anlaysis and filter the data.
+  # The syntax means the following:
+  #   If the column name does *not* start with a
+  #   minus sign '-', then the analysis should be
+  #   performed for this value.
+  #   If the column name starts with a minus sign
+  #   '-', then the data should be filtered for
+  #   all rows, where this value is 1. Filtere
+  #   means that this rows with a 1 value are
+  #   removed.
+  analysisOrder <- c("Vorhofflimmern")
+  # analysisOrder <- c("Vorhofflimmern",
+  #                    "Herzinsuffizienz",
+  #                    "-Myokardinfarkt",
+  #                    "-Schlaganfall",
+  #                    "Vorhofflimmern",
+  #                    "Herzinsuffizienz",
+  #                    "-Herzinsuffizienz",
+  #                    "Vorhofflimmern")
   
-  # plot roc curve to pdf
-  # Explanation of the graph:
-  # Sens - Sensitivity
-  # Spec - Specificity
-  # PV+  - Percentage of false negatives for VHF among all test negatives
-  # PV- - Proportion of false positives among all test positives
-  if (!hasError) {
-    roc <- ROC(test = result$NTproBNP.valueQuantity.value, stat = result$Vorhofflimmern, plot = "ROC", main = "NTproBNP(Gesamt)", AUC = TRUE)
-  }
-  
-  # start text file logging
-  cat("###########################\n")
-  cat("# Results of VHF Analysis #\n")
-  cat("###########################\n\n")
-  cat(paste0("Date: ", Sys.time(), "\n\n"))
+  for (analyisOption in analysisOrder) {
 
-  cat(comparatorFrequencies, "\n", sep = "\n")
+    resultRows <- nrow(result)
   
-  cat(paste0("Run Option: ", runOption, ifelse(filterComparatorValues, paste0(" (", removedObservationsCount, " Observations with comparator removed)"), "")), "\n\n")
-
-  # run analysis if the result table has not only 0 or 1 row and not all diagnoses values are the same
-  if (!hasError) {
+    # check possible data problems
+    errorMessage <- ""
+    # not enough data rows 
+    if (resultRows < 2) {
+      errorMessage <- paste0("Result table has ", resultRows, " rows -> abort analysis\n")
+    }
+    if (all(result$Vorhofflimmern == result$Vorhofflimmern[1])) { # only 0 or only 1 in this diagnosis column
+      errorMessage <- paste0("All Vorhofflimmern diagnoses have the same value ", result$Vorhofflimmern[1], " -> abort analysis\n")
+    }
+    hasError <- nchar(errorMessage) > 0
     
-    # print AUC to the text file
-    cat(paste0("ROC Area Under Curve NTproBNP(Gesamt): "), roc$AUC, "\n\n")
+    # plot roc curve to pdf
+    # Explanation of the graph:
+    # Sens - Sensitivity
+    # Spec - Specificity
+    # PV+  - Percentage of false negatives for VHF among all test negatives
+    # PV- - Proportion of false positives among all test positives
+    if (!hasError) {
+      roc <- ROC(test = result$NTproBNP.valueQuantity.value, stat = result$Vorhofflimmern, plot = "ROC", main = "NTproBNP(Gesamt)", AUC = TRUE)
+    }
     
-    # create different CUT points for NTproBNP
-    thresholds <- c(1 : 60) * 50
+    # start text file logging
+    cat("###########################\n")
+    cat("# Results of VHF Analysis #\n")
+    cat("###########################\n\n")
+    cat(paste0("Date: ", Sys.time(), "\n\n"))
+  
+    cat(comparatorFrequencies, "\n", sep = "\n")
     
-    cat("NtProBNP Threshold Values Analysis\n")
-    cat("----------------------------------\n\n")
-    
-    for (i in c(1 : length(thresholds))) {
-    
-      cutsColumnlName <- "NTproBNP.valueQuantity.value_cut"
-      cuts <- result[[cutsColumnlName]] <- ifelse(result$NTproBNP.valueQuantity.value < thresholds[i], 0, 1)
-    
-      cat(paste0("Threshold Value: ", thresholds[i], "\n"))
-      cat("---------------------\n")
+    cat(paste0("Run Option: ", runOption, ifelse(filterComparatorValues, paste0(" (", removedObservationsCount, " Observations with comparator removed)"), "")), "\n\n")
+  
+    # run analysis if the result table has not only 0 or 1 row and not all diagnoses values are the same
+    if (!hasError) {
       
-      CrossTable(result$Vorhofflimmern, 
-                 cuts, 
-                 prop.c = TRUE, 
-                 digits = 2, 
-                 prop.chisq = FALSE, 
-                 format = "SPSS")
+      # print AUC to the text file
+      cat(paste0("ROC Area Under Curve NTproBNP(Gesamt): "), roc$AUC, "\n\n")
       
-      if (all(cuts == cuts[1])) { # all cuts have the same value (all 0 or all 1) -> no further calculations 
-        # log information in the output file
-        cat(paste0("All NTproBNP values are greater than ", thresholds[i]," -> sensitivity, specifity, PV+ and PV- not available.\n\n\n"))
-      } else {
-        table <- xtabs(~cuts + result$Vorhofflimmern)
-        test <- rowSums(table)
-        sick <- colSums(table)
-        
-        # sensitivity
-        sensitivity <- table[2, 2] / sick[2]
-        cat(paste0("Sensitivity: ", sensitivity, "\n"))
-        
-        # specifity
-        specifity <- table[1, 1] / sick[1]
-        cat(paste0("  Specifity: ", specifity, "\n"))
-        
-        # npw - the positive predictive value
-        ppv <- table[2, 2] / test[2]
-        cat(paste0("        PV+: ", ppv, "\n"))
+      # create different CUT points for NTproBNP
+      thresholds <- c(1 : 60) * 50
       
-        # npw - Der negativepredictive value
-        npv <- table[1, 1] / test[1]
-        cat(paste0("        PV-: ", npv, "\n\n"))
-    
-        # add the ROC plot to the pdf and the AUC value to the text file
-        rocTitle <- paste0("NtproBNP_cut", thresholds[i],  " BY VHF")
-        roc <- ROC(test = cuts, stat = result$Vorhofflimmern, plot = "ROC", main = rocTitle)
-        cat(paste0("ROC Area Under Curve (Threshold ", thresholds[i], "): "), roc$AUC, "\n\n\n")
+      cat("NtProBNP Threshold Values Analysis\n")
+      cat("----------------------------------\n\n")
+      
+      for (i in c(1 : length(thresholds))) {
+      
+        cutsColumnlName <- "NTproBNP.valueQuantity.value_cut"
+        cuts <- result[[cutsColumnlName]] <- ifelse(result$NTproBNP.valueQuantity.value < thresholds[i], 0, 1)
+      
+        cat(paste0("Threshold Value: ", thresholds[i], "\n"))
+        cat("---------------------\n")
+        
+        CrossTable(result$Vorhofflimmern, 
+                   cuts, 
+                   prop.c = TRUE, 
+                   digits = 2, 
+                   prop.chisq = FALSE, 
+                   format = "SPSS")
+        
+        if (all(cuts == cuts[1])) { # all cuts have the same value (all 0 or all 1) -> no further calculations 
+          # log information in the output file
+          cat(paste0("All NTproBNP values are greater than ", thresholds[i]," -> sensitivity, specifity, PV+ and PV- not available.\n\n\n"))
+        } else {
+          table <- xtabs(~cuts + result$Vorhofflimmern)
+          test <- rowSums(table)
+          sick <- colSums(table)
+          
+          # sensitivity
+          sensitivity <- table[2, 2] / sick[2]
+          cat(paste0("Sensitivity: ", sensitivity, "\n"))
+          
+          # specifity
+          specifity <- table[1, 1] / sick[1]
+          cat(paste0("  Specifity: ", specifity, "\n"))
+          
+          # npw - the positive predictive value
+          ppv <- table[2, 2] / test[2]
+          cat(paste0("        PV+: ", ppv, "\n"))
+        
+          # npw - Der negativepredictive value
+          npv <- table[1, 1] / test[1]
+          cat(paste0("        PV-: ", npv, "\n\n"))
+      
+          # add the ROC plot to the pdf and the AUC value to the text file
+          rocTitle <- paste0("NtproBNP_cut", thresholds[i],  " BY VHF")
+          roc <- ROC(test = cuts, stat = result$Vorhofflimmern, plot = "ROC", main = rocTitle)
+          cat(paste0("ROC Area Under Curve (Threshold ", thresholds[i], "): "), roc$AUC, "\n\n\n")
+        }
       }
-    }
-    
-    #Multivarite Analyse, VHF in Abhängigkeit von NTproBNP, adjustiert mit Alter und Geschlecht
-    
-    cat("GLM Analysis\n")
-    cat("------------")
-    
-    # calculate age by birthdate and NTproBNP date
-    result$NTproBNP.date <- as.POSIXct(result$NTproBNP.date, format = "%Y")
-    result$birthdate <- as.POSIXct(result$birthdate, format = "%Y")
-    result$age <- year(result$NTproBNP.date) - year(result$birthdate)
-    
-    # glm(...) throws an error if one of the so called contrast values (vector)
-    # has always the same value -> we must identify these contrast values and
-    # remove them from our analysis
-    
-    # all contrast values we want to consider
-    contrast_names = c("NTproBNP.valueQuantity.value", "age", "gender")
-    # list for all contratst we want to use in glm(...) is filled
-    # by the given contrast column names
-    contrasts <- list()
-    for (i in 1 : length(contrast_names)) {
-      colName <- contrast_names[i] # get column name i
-      con <- result[[colName]]     # get result column with colum name i
-      contrasts[i] <- list(con)    # add result column as list item to contrasts
-    }
-    
-    # now remove all invalid constrast (= contrast
-    # vectors where all values are equal)
-    for (i in length(contrasts) : 1) {
-      con <- contrasts[i][[1]] # get the contrast vector i
-      first_con <- con[1]      # get th first element of contrast vector i
-      # if all values are equal in the contrast vector
-      if (all(con == first_con)) {
-        # log information in the output file
-        cat(paste0("All values of '", contrast_names[i], "' have the same value '", first_con, "' -> '", contrast_names[i], "' is ignored.\n"))
-        # remove the invalid contrast vector
-        contrasts <- contrasts[- i]
-      } else {
-        # The values are not all the same -> replace
-        # contrast vector by its column name in the
-        # result table. We only need the names to
-        # construct the glm(...) formula.
-        contrasts[i] <- contrast_names[i]
+      
+      #Multivarite Analyse, VHF in Abhängigkeit von NTproBNP, adjustiert mit Alter und Geschlecht
+      
+      cat("GLM Analysis\n")
+      cat("------------")
+      
+      # calculate age by birthdate and NTproBNP date
+      result$NTproBNP.date <- as.POSIXct(result$NTproBNP.date, format = "%Y")
+      result$birthdate <- as.POSIXct(result$birthdate, format = "%Y")
+      result$age <- year(result$NTproBNP.date) - year(result$birthdate)
+      
+      # glm(...) throws an error if one of the so called contrast values (vector)
+      # has always the same value -> we must identify these contrast values and
+      # remove them from our analysis
+      
+      # all contrast values we want to consider
+      contrast_names = c("NTproBNP.valueQuantity.value", "age", "gender")
+      # list for all contratst we want to use in glm(...) is filled
+      # by the given contrast column names
+      contrasts <- list()
+      for (i in 1 : length(contrast_names)) {
+        colName <- contrast_names[i] # get column name i
+        con <- result[[colName]]     # get result column with colum name i
+        contrasts[i] <- list(con)    # add result column as list item to contrasts
       }
-    }
-    
-    cat("\n")
-    
-    # construct the formula for glm(...) 
-    logit_formula <- as.formula(paste("Vorhofflimmern ~ ", paste(contrasts, collapse = "+")))
-    logit <- glm(logit_formula, family = binomial, data = result)
-    summaryText <- capture.output(summary(logit)) # https://www.r-bloggers.com/2015/02/export-r-output-to-a-file/
-    cat(summaryText, sep = "\n") # summaryText is a list -> print list with line breaks   
-  } else {
-    cat(errorMessage)
-    message(errorMessage)
-  } 
+      
+      # now remove all invalid constrast (= contrast
+      # vectors where all values are equal)
+      for (i in length(contrasts) : 1) {
+        con <- contrasts[i][[1]] # get the contrast vector i
+        first_con <- con[1]      # get th first element of contrast vector i
+        # if all values are equal in the contrast vector
+        if (all(con == first_con)) {
+          # log information in the output file
+          cat(paste0("All values of '", contrast_names[i], "' have the same value '", first_con, "' -> '", contrast_names[i], "' is ignored.\n"))
+          # remove the invalid contrast vector
+          contrasts <- contrasts[- i]
+        } else {
+          # The values are not all the same -> replace
+          # contrast vector by its column name in the
+          # result table. We only need the names to
+          # construct the glm(...) formula.
+          contrasts[i] <- contrast_names[i]
+        }
+      }
+      
+      cat("\n")
+      
+      # construct the formula for glm(...) 
+      logit_formula <- as.formula(paste("Vorhofflimmern ~ ", paste(contrasts, collapse = "+")))
+      logit <- glm(logit_formula, family = binomial, data = result)
+      summaryText <- capture.output(summary(logit)) # https://www.r-bloggers.com/2015/02/export-r-output-to-a-file/
+      cat(summaryText, sep = "\n") # summaryText is a list -> print list with line breaks   
+    } else {
+      cat(errorMessage)
+      message(errorMessage)
+    } 
+  }
 }
-
+  
 logText <- paste0("\nFinished: ", Sys.time(), "\n")
 cat(logText)
 message(logText)
